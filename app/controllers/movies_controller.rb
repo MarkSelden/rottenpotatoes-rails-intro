4904994f484b72f53010813f69fbb1c5cf0b975f
Mark Selden <seldenm@berkeley.edu>
2020-10-21 05:34:07 +0000

    debug time

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,6 @@ class MoviesController < ApplicationController
   end
 
   def index
-    byebug
     @all_ratings = Movie.@all_ratings
     @ratings_to_show = params[:ratings].keys || @all_ratings
     @movies = Movie.with_ratings(@ratings_to_show)
