e7bced17067a8a075edb08c0075b1f56c6d999fc
Mark Selden <seldenm@berkeley.edu>
2020-10-21 05:53:56 +0000

    removed do

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,11 +1,11 @@
 class Movie < ActiveRecord::Base
   @@all_ratings = ['G', 'PG', 'PG-13', 'R']
   
-  def self.all_ratings do
+  def self.all_ratings 
     @@all_ratings
   end
   
-  def self.with_ratings(ratings_list) do
+  def self.with_ratings(ratings_list) 
     Movie.where(rating: ratings_list)
   end 
 end
